8dda0d75a95b88a2c241377bb41fac97b6d5b0a8
WonderSTK <mehulparmar9694@gmail.com>
2024-12-10 15:30:33 +0530

    add function to calculate the sum of all numbers in an array and implement diffArr function to find differences between two arrays

--- sum_of_all_numbers_in_range.js
+++ sum_of_all_numbers_in_range.js
@@ -7,3 +7,14 @@ const sumOfAllNumbersInRange = (start, end) => {
 };
 
 console.log(sumOfAllNumbersInRange(1, 10)); // 55
+
+
+//
+
+function sumAll(arr) {
+  let sum = 0;
+  for (let i = Math.min(...arr); i <= Math.max(...arr); i++) {
+    sum += i;
+  }
+  return sum;
+}
